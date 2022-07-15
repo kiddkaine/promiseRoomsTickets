bd44412a4128b87dc0b5d8ca874f21e3ff9e75f6
Kidd Kaine <79826408+kiddkaine@users.noreply.github.com>
2022-07-15 16:56:38 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 const success = (data) => {
-
 	console.log('Ответ от сервера:', data);
-
 	console.log('Едем в отпуск! :)');
-
 }
 
 const failed = (data) => {
-
 	console.log('Ответ от сервера:', data);
-
 	console.log('Не едем в отпуск... :(');
+}
 
+function checkData () {
+	return new Promise ((resolve, reject) => {
+		console.log('Ассинхронная проверка данных Promise');
+		console.log('--- Проверка номеров в отеле ---');
+		setTimeout(() => {
+			const response = true;
+			if (response) {
+				let message = 'Номера в отеле имеются!';
+				resolve(message);
+			} else {
+				let message = 'Номера в отеле отсутствуют...';
+				reject(message);
+			}
+		}, 1000);
+	});
 }
 
 const checkTickets = (data) => {
-
 	console.log('Ответ от сервера:', data);
-
 	console.log('--- Проверка авиабилетов ---');
-
 	return new Promise ((resolve, reject) => {
-
 		setTimeout(() => {
-
 			const response = true;
-
 			if (response) {
 				let message = 'Авиабилеты имеются!';
 				resolve(message);
@@ -34,27 +39,7 @@ const checkTickets = (data) => {
 				reject(message);
 			}
 		}, 2000);
-
 	});
-	
 }
 
-const checkData = new Promise ((resolve, reject) => {
-
-	console.log('Ассинхронная проверка данных Promise');
-
-	console.log('--- Проверка номеров в отеле ---');
-
-	setTimeout(() => {
-		const response = true;
-
-		if (response) {
-			let message = 'Номера в отеле имеются!';
-			resolve(message);
-		} else {
-			let message = 'Номера в отеле отсутствуют...';
-			reject(message);
-		}
-	}, 1000);
-
-}).then(checkTickets).then(success).catch(failed);
+checkData().then(checkTickets).then(success).catch(failed);
