6199044f5c4f3f2439dbacd7936c84115cff547a
Kidd Kaine <79826408+kiddkaine@users.noreply.github.com>
2022-07-15 17:28:52 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-const success = (data) => {
-	console.log('Ответ от сервера:', data);
-	console.log('Едем в отпуск! :)');
-}
-
-const failed = (data) => {
-	console.log('Ответ от сервера:', data);
-	console.log('Не едем в отпуск... :(');
-}
-
 function checkRooms () {
 	return new Promise ((resolve, reject) => {
 		console.log('Ассинхронная проверка данных Promise');
@@ -25,7 +15,7 @@ function checkRooms () {
 const checkTickets = (data) => {
 	console.log('Ответ от сервера:', data);
 	console.log('--- Проверка авиабилетов ---');
-	
+
 	return new Promise ((resolve, reject) => {
 		setTimeout(() => {
 			const response = true;
@@ -36,7 +26,30 @@ const checkTickets = (data) => {
 	});
 }
 
-checkRooms()
-	.then(checkTickets)
-	.then(success)
-	.catch(failed);
+const success = (data) => {
+	console.log('Ответ от сервера:', data);
+	console.log('Едем в отпуск! :)');
+}
+
+const failed = (data) => {
+	console.log('Ответ от сервера:', data);
+	console.log('Не едем в отпуск... :(');
+}
+
+// checkRooms()
+// 	.then(checkTickets)
+// 	.then(success)
+// 	.catch(failed);
+
+async function checkVacation() {
+	try {
+		const roomsResult = await checkRooms();
+		const ticketsResult = await checkTickets(roomsResult);
+		
+		success(ticketsResult);
+	} catch (error) {
+		failed(error);
+	}
+}
+
+checkVacation();
